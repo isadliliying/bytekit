f7933a4407230309bf64e9d197610329b3fb1593
isadliliying@163.com <isadliliying@163.com>
2023-01-28 22:38:20 +0800

    add compare test and comment

--- bytekit-core/src/test/java/com/alibaba/bytekit/asm/location/LineBeforeLocationMatcherEnhanceTest.java
+++ bytekit-core/src/test/java/com/alibaba/bytekit/asm/location/LineBeforeLocationMatcherEnhanceTest.java
@@ -5,7 +5,9 @@ import com.alibaba.bytekit.asm.binding.Binding;
 import com.alibaba.bytekit.asm.interceptor.InterceptorProcessor;
 import com.alibaba.bytekit.asm.interceptor.annotation.InterceptorParserUtils;
 import com.alibaba.bytekit.asm.interceptor.annotation.None;
-import com.alibaba.bytekit.utils.*;
+import com.alibaba.bytekit.utils.AsmUtils;
+import com.alibaba.bytekit.utils.Decompiler;
+import com.alibaba.bytekit.utils.ReflectionUtils;
 import com.alibaba.deps.org.objectweb.asm.tree.ClassNode;
 import com.alibaba.deps.org.objectweb.asm.tree.MethodNode;
 import org.junit.Rule;
@@ -14,32 +16,31 @@ import org.springframework.boot.test.rule.OutputCapture;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineBeforeLocationMatcherEnhanceTest {
 
     @Rule
     public OutputCapture capture = new OutputCapture();
 
-    public static class SpyLookInterceptor {
+    public static class SpyLookInterceptorFail {
 
-        public static void atLineBefore(@Binding.This Object target, @Binding.Class Class<?> clazz,
-                                        @Binding.MethodInfo String methodInfo, @Binding.Args Object[] args,
-                                        @Binding.LineBefore int line,
-                                        @Binding.LocalVars Object[] vars,
+        public static void atLineBefore(@Binding.LocalVars Object[] vars,
                                         @Binding.LocalVarNames String[] varNames) {
-            System.out.println("atLineBefore-args:" + clazz + methodInfo + target + args + line + vars + varNames);
+            System.out.println("atLineBefore-args:" + vars + varNames);
         }
 
     }
 
+    public static class SpyLookInterceptorSuccess {
+
+        public static void atLineBefore(@Binding.LocalVars(excludePattern = "*$*") Object[] vars,
+                                        @Binding.LocalVarNames(excludePattern = "*$*") String[] varNames) {
+            System.out.println("atLineBefore-args:" + vars + varNames);
+        }
+
+    }
 
     /**
      * 使用了Binding.LocalVars增强时，会报错
@@ -51,9 +52,23 @@ public class LineBeforeLocationMatcherEnhanceTest {
      */
     @Test
     public void testLocalVarsEnhanceError() throws Exception {
+        testLocalVarsEnhance(SpyLookInterceptorFail.class);
+    }
+
+    /**
+     * 经过大佬的排查，发现某些版本kotlin下编译时，会在LocalVariableTable写入一些在字节码中不会使用到的变量（不会读取也不会赋值）
+     * 而这部分变量在作为被binding使用时，就会出现问题
+     * 这些特殊生成的变量一般也不需要进行观测，所以希望添加exclude，把这部分去掉，也能避免报错
+     */
+    @Test
+    public void testLocalVarsEnhanceSuccess() throws Exception {
+        testLocalVarsEnhance(SpyLookInterceptorSuccess.class);
+    }
+
+    public void testLocalVarsEnhance(Class<?> clazz) throws Exception {
 
         LineBeforeLocationMatcher locationMatcher = new LineBeforeLocationMatcher(7);
-        Method method = ReflectionUtils.findMethod(SpyLookInterceptor.class, "atLineBefore", null);
+        Method method = ReflectionUtils.findMethod(clazz, "atLineBefore", null);
         InterceptorProcessor lineBeforeInterceptorProcessor = InterceptorParserUtils.createInterceptorProcessor(method, locationMatcher, true, None.class, Void.class);
 
         String fileName = this.getClass().getClassLoader().getResource("TestManager.class").getFile();
